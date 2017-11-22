d2e485ca70ae2c1f511fadc4432e73a93cf40e3a
talant <talantruzmetov@gmail.com>
2017-11-22 18:23:51 -0500

    diff maze topology

--- src/main/java/assignment4/EasyGridWorldLauncher.java
+++ src/main/java/assignment4/EasyGridWorldLauncher.java
@@ -29,16 +29,16 @@ public class EasyGridWorldLauncher {
 	private static boolean showPolicyIterationPolicyMap = true;
 	private static boolean showQLearningPolicyMap = true;
 	
-	private static Integer MAX_ITERATIONS = 100;
+	private static Integer MAX_ITERATIONS = 300;
 	private static Integer NUM_INTERVALS = 100;
 
 	protected static int[][] userMap = new int[][] { 
-	    { 1, 0, 0, 0, 0, 0},
-	    { 0, 0, 1, 0, 1, 0},
-	    { 1, 0, 1, 0, 1, 0},
-	    { 0, 0, 1, 0, 1, 0},
-	    { 1, 0, 0, 0, 0, 0},
-	    { 0, 0, 0, 1, 0, 1},};
+	    { 0, 0, 0, 0, 0, 0},
+	    { 0, 1, 1, 0, 1, 0},
+	    { 0, 1, 0, 0, 1, 0},
+	    { 0, 1, 1, 0, 1, 0},
+	    { 0, 1, 1, 0, 1, 0},
+	    { 0, 0, 0, 0, 0, 0},};
 	
 //	private static Integer mapLen = map.length-1;
 
